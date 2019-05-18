cd864c139939666788e0f1a31e91a9c84932b01e
norbertkadar <norbert.kadar.a@gmail.com>
2019-05-18 09:37:07 +0300

    comment

--- Lab1/Controllers/ValuesController.cs
+++ Lab1/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace Lab1.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "testValue", "value2" };
         }
 
         // GET api/values/5
